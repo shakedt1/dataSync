aa8ecfa7c667c18ff426444fa6bbec67b45ea732
shakedt1 <r1416@bamaale.ort.org.il>
2022-01-17 14:25:22 +0200

    fix some issues

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     node_manager.add_node([])
     node_manager.add_node([0])
     node_manager.add_node([0, 1])
+    node_manager.add_node([0, 1, 5])
 
     sleep(5)
 
@@ -13,3 +14,10 @@ if __name__ == "__main__":
 
     sleep(1)
     node_manager.add_node([1])
+    node_manager.add_node([3, 2])
+
+    sleep(2)
+
+    node_manager.remove_node(1)
+
+    sleep(6)
--- node_manager.py
+++ node_manager.py
@@ -7,11 +7,11 @@ last_id = 0
 def add_node(neighbors):
     global last_id
 
-    for neighbor in neighbors:
-        if neighbor not in nodes:
-            print("No such neighbors")
-            return
-        nodes[neighbor].add_neighbor(last_id)
+    if not set(neighbors).issubset(nodes.keys()):
+        print("No such neighbors")
+        return
+
+    [nodes[neighbor].add_neighbor(last_id) for neighbor in neighbors]
 
     node = Node(last_id, neighbors)
     nodes[last_id] = node
@@ -20,6 +20,9 @@ def add_node(neighbors):
 
 
 def remove_node(id):
+    if id not in nodes:
+        print("No such node")
+        return
     [node.remove_neighbor(id) for node in nodes.values() if id in node.neighbors]
     nodes[id].stop_node()
     del nodes[id]
